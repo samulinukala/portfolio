2712664601986fa663b82f35769846fb951419ba
samulinukala <samulinukala740@gmail.com>
2026-05-06 17:12:37 +0300

    lazy commit while troubleshooting

--- src/App.tsx
+++ src/App.tsx
@@ -122,10 +122,11 @@ const onSubmit=e=>
  
   async function tryLogin()
 {
-  await fetch("https://portfolio-backend-tur1.onrender.com/api/users/login/"+username+"/"+password,
+  const d=await fetch("https://portfolio-backend-tur1.onrender.com/api/users/login/"+username+"/"+password,
 {
 credentials: 'include',
 });
+props.setCookie("userTokenJwt",d);
 }
 tryLogin();
 }
